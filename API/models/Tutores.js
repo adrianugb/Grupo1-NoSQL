f9338f0f1c861c475fb81e695ab7d1014120d734
Geovanny Arias <geoas121cr@gmail.com>
2025-07-10 12:50:56 -0600

    Add Tutores model schema for managing tutor data

---
+++ API/models/Tutores.js
@@ -0,0 +1,17 @@
+﻿const mongoose = require('mongoose');
+
+const TutoresSchema = new mongoose.Schema(
+    {
+        //id: { type: Number, required: true, unique: true},
+        id: { type: Number},
+        nombre: { type: String},
+        correo: { type: String},
+        especialidades: [{ type: String}],
+        disponibilidad: [{ type: String}],
+        activo: { type: Boolean, default: true},
+        fecha_registro: { type: Date, default: Date.now}
+    }
+);
+
+
+module.exports = mongoose.model('Tutores', TutoresSchema);
